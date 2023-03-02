7031fad8b27f22dcf5c7b71bd51d28b824b5f9bd
Theeramet Metha <theeramet.m@apollo21.asia>
2023-03-02 07:12:45 +0700

    quite ok

---
+++ ClassProcessing.py
@@ -0,0 +1,42 @@
+from flask_restful import Resource
+import json
+from flask import Response, request
+import requests
+from Const import api_url_for_get_post, api_url_for_post_movie_list
+from ClassFindMovie import FindMovie
+from FuncFindMovieName import FindMovieNameByTMDBId
+from itertools import islice
+
+class Process(Resource):
+  def get(self):
+
+    req = requests.get(api_url_for_get_post)
+    data = req.json()
+      
+    if(len(data) > 0):
+      movieDict = FindMovie.findWithDetail(postDetail=data['postDetail'])
+      movieList = (createMovieObjList(movieDict))
+      body = {
+        "postId":data['postId'],
+        "movielist":movieList
+      }
+      requests.post(api_url_for_post_movie_list, json=body)
+      
+      l = json.dumps(body)
+      return json.loads(l)
+    else:
+      return Response("No new post to process", status=204, mimetype='application/json')
+  
+def createMovieObjList(movieDict):
+    mylist = []
+    for key, value in movieDict.items():
+      mylist.append(createMovieObj(key, value))
+    return mylist[0:3]
+  
+def createMovieObj(tmdbId, freq):
+    obj = {
+      "tmdbId":tmdbId,
+      "name":FindMovieNameByTMDBId(tmdbId),
+      "freq":freq
+    }
+    return obj
--- server.py
+++ server.py
@@ -3,24 +3,18 @@ from flask import Flask
 from flask_restful import Api
 from flask_cors import CORS
 
-from FuncWordTokenize import word_tokenize
-from ClassProcess import Process
-
-#==== Class method Post ============================================
-from ClassTriggerHasNewPost import TriggerHasNewPost
+from ClassProcessing import Process
 
 app = Flask(__name__)
 api = Api(app)
 CORS(app)
 
-# POST 
-#api.add_resource(TriggerHasNewPost,"/hasnewpost") #support {params:{ a:1, b:2}}
-#api.add_resource(Process,"/hasnewpost") #support {params:{ a:1, b:2}}
+api.add_resource(Process,"/start-one-process") 
 
 if __name__ == "__main__":
     app.run(debug=True, port=5001)
     #Change debug to false when deploy
     
-    #=== Main Process ==================================================
+    
     
     
